7085414476d5e9fc4b604d5c5b2920e9b966b644
Nguyễn Thuận <nguyenthuan@Nguyens-MacBook-Pro-4.local>
2021-08-16 15:22:37 +0700

    ready try to deploying

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
-const express = require("express")
-const userRouter = require("./modules/user/user.router")
-const productRouter = require("./modules/product/product.router")
-const mongoose = require("mongoose")
-require('dotenv').config()
-const cors = require("cors")
-app = express() 
+const express = require("express");
+const userRouter = require("./modules/user/user.router");
+const productRouter = require("./modules/product/product.router");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+app = express();
 
-mongoose.connect('mongodb://localhost:27017/shopping-clothes', {useNewUrlParser: true}, (err) => {
-    if (err) throw err
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/shopping-clothes",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (err) throw err;
 
-    console.log("MongoDB connected")
-});
+    console.log("MongoDB connected");
+  }
+);
 
 app.use(cors());
-  
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use('/user', userRouter)
-app.use('/product', productRouter)
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/product", productRouter);
 
-const port = process.env.APP_PORT
+const port = process.env.APP_PORT || 8080;
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`) 
-})
+  console.log(`Server started on port ${port}`);
+});
