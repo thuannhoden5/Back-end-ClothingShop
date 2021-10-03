88285d7f3dd5fd5e5f3dbbd40e513fac5df0dcb5
Nguyễn Thuận <nguyenthuan@Nguyens-MacBook-Pro.local>
2021-10-03 17:57:09 +0700

    chagne app_port -> port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const userRouter = require('./modules/user/user.router');
-const cartRouter = require('./modules/cart/cart.router');
-const productRouter = require('./modules/product/product.router');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const cors = require('cors');
-const commentRouter = require('./modules/comment/commentt.router');
+const express = require("express");
+const userRouter = require("./modules/user/user.router");
+const cartRouter = require("./modules/cart/cart.router");
+const productRouter = require("./modules/product/product.router");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const commentRouter = require("./modules/comment/commentt.router");
 const orderRouter = require("./modules/order/order.router");
 
 app = express();
@@ -21,20 +21,20 @@ mongoose.connect(
   (err) => {
     if (err) throw err;
 
-    console.log('MongoDB connected');
-  },
+    console.log("MongoDB connected");
+  }
 );
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/user', userRouter);
-app.use('/product', productRouter);
-app.use('/cart', cartRouter);
-app.use('/order', orderRouter);
-app.use('/comment', commentRouter);
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+app.use("/cart", cartRouter);
+app.use("/order", orderRouter);
+app.use("/comment", commentRouter);
 
-const port = process.env.APP_PORT || 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
