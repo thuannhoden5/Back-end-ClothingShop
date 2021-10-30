2e3f0b709cadec6f6e96a336ae9ef049d25a4833
Le Hai Thanh <haithuthanhtien@gmail.com>
2021-10-30 10:58:40 +0700

    update deploy

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const userRouter = require("./modules/user/user.router");
-const cartRouter = require("./modules/cart/cart.router");
-const productRouter = require("./modules/product/product.router");
-const mongoose = require("mongoose");
-const commentRouter = require("./modules/comment/commentt.router");
-const orderRouter = require("./modules/order/order.router");
-require("dotenv").config();
-const cors = require("cors");
+const express = require('express');
+const userRouter = require('./modules/user/user.router');
+const cartRouter = require('./modules/cart/cart.router');
+const productRouter = require('./modules/product/product.router');
+const mongoose = require('mongoose');
+const commentRouter = require('./modules/comment/commentt.router');
+const orderRouter = require('./modules/order/order.router');
+require('dotenv').config();
+const cors = require('cors');
 
 app = express();
 
@@ -21,18 +21,22 @@ mongoose.connect(
   (err) => {
     if (err) throw err;
 
-    console.log("MongoDB connected");
-  }
+    console.log('MongoDB connected');
+  },
 );
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use("/user", userRouter);
-app.use("/product", productRouter);
-app.use("/cart", cartRouter);
-app.use("/order", orderRouter);
-app.use("/comment", commentRouter);
+app.use('/user', userRouter);
+app.use('/product', productRouter);
+app.use('/cart', cartRouter);
+app.use('/order', orderRouter);
+app.use('/comment', commentRouter);
+
+app.get('/hello', async (req, res) => {
+  res.send('hello');
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
